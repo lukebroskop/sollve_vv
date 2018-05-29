b2c551f79dc26fda156750e95029a06e600a485e
Jose Manuel Monsalve Diaz <josem@udel.edu>
2018-05-29 17:18:49 -0400

    Cleaning up tests/target_teams_distribute/test_target_teams_distribute_is_device_ptr.c

--- tests/target_teams_distribute/test_target_teams_distribute_is_device_ptr.c
+++ tests/target_teams_distribute/test_target_teams_distribute_is_device_ptr.c
@@ -4,52 +4,39 @@
 #include "ompvv.h"
 
 #define SIZE_THRESHOLD 512
+#define ARRAY_SIZE 1024
 
 // Test for OpenMP 4.5 target data with if
 int main() {
-  int isOffloading = 0;
-  OMPVV_TEST_AND_SET_OFFLOADING(isOffloading);
-  int a[1024];
-  int b[1024];
-  int *c = (int *)omp_target_alloc(1024 * sizeof(int), omp_get_default_device());
+  OMPVV_TEST_OFFLOADING;
+  OMPVV_INFOMSG("test target_teams_distribute_is_device_ptr");
+
+  int a[ARRAY_SIZE];
+  int b[ARRAY_SIZE];
+  int *c = (int *)omp_target_alloc(ARRAY_SIZE * sizeof(int), omp_get_default_device());
   int errors = 0;
   if (!c){
       OMPVV_WARNING("Test was unable to allocate memory on device.  Test could not procede.");
       OMPVV_REPORT_AND_RETURN(errors);
-  }
-  else{
-      int devtest = 1;
-      #pragma omp target enter data map(to: devtest)
-      #pragma omp target
-      {
-          devtest = 0;
-      }
+  } 
+  else {
       // a and b array initialization
-      for (int x = 0; x < 1024; ++x) {
+      for (int x = 0; x < ARRAY_SIZE; ++x) {
           a[x] = 1;
           b[x] = x;
       }
 
-      #pragma omp target data map(tofrom: a[0:1024]) map(to: b[0:1024])
+      #pragma omp target data map(tofrom: a[0:ARRAY_SIZE]) map(to: b[0:ARRAY_SIZE])
       {
           #pragma omp target teams distribute is_device_ptr(c)
-          for (int x = 0; x < 1024; ++x){
+          for (int x = 0; x < ARRAY_SIZE; ++x){
               c[x] = b[x] * b[x];
               a[x] += c[x] + b[x];
           }
       }
 
-      for (int x = 0; x < 1024; ++x){
+      for (int x = 0; x < ARRAY_SIZE; ++x){
           OMPVV_TEST_AND_SET(errors, (a[x] != 1 + b[x] + b[x] * b[x]));
-      }
-
-
-      if (!errors) {
-        OMPVV_INFOMSG("Test passed with offloading %s", (isOffloading ? "enabled" : "disabled"));
-      } else if (devtest == 1) {
-        OMPVV_ERROR("Test failed on device with offloading %s.", (isOffloading ? "enabled" : "disabled"));
-      } else if (devtest == 0) {
-        OMPVV_ERROR("Test failed on host with offloading %s.", (isOffloading ? "enabled" : "disabled"));
       }
 
       OMPVV_REPORT_AND_RETURN(errors);
