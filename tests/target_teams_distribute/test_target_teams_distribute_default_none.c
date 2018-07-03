090d9d8fe35d74453657d7e4f0ab0f9e0275c119
Kyle <kylefriedline@gmail.com>
2018-07-03 18:17:34 -0400

    Added test_target_teams_distribute_default tests

---
+++ tests/target_teams_distribute/test_target_teams_distribute_default_none.c
@@ -0,0 +1,68 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ompvv.h"
+
+#define SIZE_THRESHOLD 512
+
+// Test for OpenMP 4.5 target data with if
+int main() {
+  int isOffloading = 0;
+  OMPVV_TEST_AND_SET_OFFLOADING(isOffloading);
+  int a[1024];
+  int b[1024];
+  int c[1024];
+  int d[1024];
+  int privatized;
+  int share;
+  int x;
+  int errors = 0;
+
+  // a and b array initialization
+  for (int x = 0; x < 1024; ++x) {
+      a[x] = 1;
+      b[x] = x;
+      c[x] = 2*x;
+      d[x] = 0;
+  }
+
+  #pragma omp target data map(from: d[0:1024]) map(to: a[0:1024], b[0:1024], c[0:1024])
+  {
+      #pragma omp target teams distribute default(none) shared(a, b, c, d) private(x, privatized)
+      for (x = 0; x < 1024; ++x){
+          privatized = 0;
+          for (int y = 0; y < a[x] + b[x]; ++y){
+              privatized++;
+          }
+          d[x] = c[x] * privatized;
+      }
+  }
+
+  for (int x = 0; x < 1024; ++x){
+      OMPVV_TEST_AND_SET_VERBOSE(errors, (d[x] != (1 + x)*2*x));
+  }
+
+  #pragma omp target data map(to: b[0:1024]) map(tofrom: share)
+  {
+      #pragma omp target teams distribute default(none) private(x) shared(share, b)
+      for (x = 0; x < 1024; ++x){
+          #pragma omp atomic
+          share = share + b[x];
+      }
+  }
+
+  for (int x = 0; x < 1024; ++x){
+      share = share - x;
+  }
+  if (share != 0){
+      OMPVV_TEST_AND_SET_VERBOSE(errors, (share != 0));
+  }
+
+  if (!errors) {
+    OMPVV_INFOMSG("Test passed with offloading %s", (isOffloading ? "enabled" : "disabled"));
+  } else {
+    OMPVV_ERROR("Test failed with offloading %s.", (isOffloading ? "enabled" : "disabled"));
+  }
+
+  OMPVV_REPORT_AND_RETURN(errors);
+}
---
+++ tests/target_teams_distribute/test_target_teams_distribute_default_shared.c
@@ -0,0 +1,67 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ompvv.h"
+
+#define SIZE_THRESHOLD 512
+
+// Test for OpenMP 4.5 target data with if
+int main() {
+  int isOffloading = 0;
+  OMPVV_TEST_AND_SET_OFFLOADING(isOffloading);
+  int a[1024];
+  int share = 0;
+  int errors = 0;
+  int num_teams;
+
+  // a and b array initialization
+  for (int x = 0; x < 1024; ++x) {
+      a[x] = x;
+  }
+
+  #pragma omp target data map(to: a[0:1024]) map(tofrom: share, num_teams)
+  {
+      #pragma omp target teams distribute default(shared)
+      for (int x = 0; x < 1024; ++x){
+          num_teams = omp_get_num_teams();
+          #pragma omp atomic
+          share = share + a[x];
+      }
+  }
+
+  for (int x = 0; x < 1024; ++x){
+      share = share - x;
+  }
+  OMPVV_TEST_AND_SET_VERBOSE(errors, (share != 0));
+
+  share = -1;
+  #pragma omp target data map(to: a[0:1024]) map(tofrom: share)
+  {
+      #pragma omp target teams distribute default(shared)
+      for (int x = 0; x < 1024; ++x){
+          share = a[x];
+      }
+  }
+
+  OMPVV_TEST_AND_SET_VERBOSE(errors, (share < 0 || share >= 1024));
+
+  share = 5;
+
+  #pragma omp target data map(tofrom: a[0:1024]) map(tofrom: share)
+  {
+      #pragma omp target teams distribute default(shared)
+      for (int x = 0; x < 1024; ++x){
+          a[x] = a[x] + share;
+      }
+  }
+
+  for (int x = 0; x < 1024; ++x){
+      OMPVV_TEST_AND_SET_VERBOSE(errors, (a[x] - 5 != x));
+  }
+
+  if (num_teams == 1){
+      OMPVV_WARNING("Test operated on one team, results of default shared test are inconclusive.");
+  }
+
+  OMPVV_REPORT_AND_RETURN(errors);
+}
