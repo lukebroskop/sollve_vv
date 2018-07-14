0e8b277db8f1f0e3f03cd3b7cbb7fc5a5927bcae
Jose Manuel Monsalve Diaz <josem@udel.edu>
2018-07-13 23:54:10 -0400

    on the tests/target/test_target_firstprivate.F90 it is better if we check the variable and modify the array instead of modifying the compute_array with the variable. This is because if the value of p_val is not the expected this might cause a segfault. It is better to fail gracefully Also, added an infomessage regarding the number of threads being used and the max number of threads that it could use. the variable actualThreadCnt has been priva shared explicitely in the parallel region

--- tests/target/test_target_firstprivate.F90
+++ tests/target/test_target_firstprivate.F90
@@ -35,7 +35,7 @@
             compute_array(:,:) = 0
 
             call omp_set_num_threads(NUM_THREADS_TEST)
-            !$omp parallel private(p_val)
+            !$omp parallel private(p_val) shared(actualThreadCnt)
               ! each p_val is private to a thread
               p_val = omp_get_thread_num() + 1
               actualThreadCnt = omp_get_num_threads()
@@ -48,12 +48,18 @@
               !$omp end target
               ! Testing that the target region did not modify the original value
               ! of p_val
-              compute_array(:, p_val) = compute_array(:, p_val) + 1
+              IF (p_val == omp_get_thread_num() + 1) THEN
+                compute_array(:, p_val) = compute_array(:, p_val) + 1
+              END IF
             !$omp end parallel 
+
+            WRITE(messageHelper, '(A,I0,A,I0)') "Test ran with ", &
+            actualThreadCnt, " threads out of ", omp_get_thread_limit()
+            OMPVV_INFOMSG(messageHelper)
 
             WRITE(messageHelper, *) "The number of threads in the &
             & host is 1. Test is inconclusive"
-            OMPVV_WARNING_IF(actualThreadCnt == 1, messageHelper)
+            OMPVV_WARNING_IF(actualThreadCnt <= 1, messageHelper)
 
             OMPVV_TEST_VERBOSE(ANY(compute_array(:,1:actualThreadCnt) /= 101))
 
