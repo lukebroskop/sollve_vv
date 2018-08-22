62db61d264ab98066af17044f26ec334f0828da7
Jose Manuel Monsalve Diaz <josem@udel.edu>
2018-08-22 14:15:01 -0400

    Splitting up test_target_data_map_devices.F90 into two tests. One for the set_default_device API call and the other one for the device clause. These two tests are different to each other. and they should also be separated in C. Other corrections to the tests were made so it could be compiled

--- tests/target_data/test_target_data_map_set_default_device.F90
+++ tests/target_data/test_target_data_map_set_default_device.F90
@@ -1,10 +1,10 @@
-!===---- test_target_data_map_devices.F90 - using target data with multiple dev --===//
+!===---- test_target_data_map_set_default_device.F90 - set_default_device() API --===//
 ! 
 ! OpenMP API Version 4.5 Nov 2015
 ! 
-! Iterating over the number of devices, this tests uses target data with
-! multiple devices using the devices clause !
-! 
+! This test check if by calling the set_default_device the target_data
+! is mapped to the default device selected by this API call
+!
 !===-------------------------------------------------------------------------===//
 #include "ompvv.F90"
 
@@ -33,33 +33,39 @@
         OMPVV_WARNING_IF(num_dev == 1, msgHelper)
 
         !starting the tests
-        OMPVV_TEST_VERBOSE(test_device_clause() .NE. 0)
+        OMPVV_TEST_VERBOSE(test_set_default_device() .NE. 0)
 
         OMPVV_REPORT_AND_RETURN()
 
         CONTAINS 
-          ! Testing set device with deviceclause
-          INTEGER FUNCTION test_device_clause()
-          INTEGER :: dev_data, dev_comp
-          INTEGER :: errors_bf, errors_af
-          INTEGER, dimension(N) :: anArray
+          ! Testing set default device API
+          INTEGER FUNCTION test_set_default_device()
+            INTEGER :: def_dev, dev_data, dev_comp
+            INTEGER :: errors_bf, errors_af
+            INTEGER, dimension(N) :: anArray
 
-          OMPVV_INFOMSG("using device() clause")
+            OMPVV_INFOMSG("using set_default_device")
 
+            def_dev = omp_get_default_device()
+            WRITE(msgHelper, '(A,I0)') "default device = ", def_dev
+            OMPVV_INFOMSG(msgHelper)
+            
             ! Initialize the array
             anArray(:) = 1
 
             ! Iterate over all the devices
-            DO dev_data = 0, num_dev - 1
-              !$omp target data map(tofrom: anArray(1:N)) &
-              !$omp device(dev_data)
+            DO dev_data = 0, (num_dev - 1)
+              ! set default device
+              CALL omp_set_default_device(dev_data)
+              !$omp target data map(tofrom: anArray(1:N))
 
-              ! Iterate over all the devices doing comp
-              ! to guarantee that the right data env is 
-              ! being copied back and forth
-                DO dev_comp = 0, num_dev - 1
+                ! Iterate over all the devices doing comp
+                ! to guarantee that the right data env is 
+                ! being copied back and forth
+                DO dev_comp = 0, (num_dev - 1)
                   !$omp target map(alloc: anArray(1:N)) &
                   !$omp device(dev_comp)
+
                     ! Increase only in the current tested device
                     IF (dev_comp == dev_data) THEN
                       anArray(1:N) = anArray(1:N) + 1
@@ -68,18 +74,23 @@
                       ! other devices
                       anArray(1:N) = anArray(1:N) - 10
                     END IF
+                    
                   !$omp end target
                 END DO ! dev_comp
-             !$omp end target data
+              !$omp end target data
             END DO ! dev_data
               
             OMPVV_GET_ERRORS(errors_bf)
             OMPVV_TEST_VERBOSE(ANY(anArray /= num_dev + 1))
             OMPVV_GET_ERRORS(errors_af)
 
-            test_device_clause = errors_bf - errors_af
+            ! return the default device to the 
+            ! original default 
+            CALL omp_set_default_device(def_dev)
 
-          END FUNCTION test_device_clause
+            test_set_default_device = errors_bf - errors_af
+
+          END FUNCTION test_set_default_device
       END PROGRAM
 
   
