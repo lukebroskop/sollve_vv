30e2286188d86da25bae3b53efb75a808a4f2021
Kyle <kylefriedline@gmail.com>
2019-01-21 22:31:37 -0500

    Made changes discussed on last weeks call

--- tests/target_teams_distribute/test_target_teams_distribute_nowait.c
+++ tests/target_teams_distribute/test_target_teams_distribute_nowait.c
@@ -15,7 +15,7 @@
 #include "ompvv.h"
 
 #define SIZE_THRESHOLD 512
-
+#define ITERATIONS 1024
 int main() {
   int isOffloading = 0;
   OMPVV_TEST_AND_SET_OFFLOADING(isOffloading);
@@ -25,61 +25,43 @@ int main() {
   int d[1024];
   int e[1024];
   int f[1024];
-  int g[1024];
   int errors = 0;
   int is_host;
+  int race_condition_found = 0;
 
-  for (int x = 0; x < 1024; ++x) {
-      a[x] = x;
-      b[x] = 2 * x;
-      c[x] = 0;
-      d[x] = 3 * x;
-      e[x] = 4 * x;
-      f[x] = 0;
-      g[x] = 0;
-  }
+  for (int y = 0; y < ITERATIONS; ++y){
+      for (int x = 0; x < 1024; ++x) {
+          a[x] = x + y;
+          b[x] = 2 * x + y;
+          c[x] = 0;
+          d[x] = 3 * x + y;
+          e[x] = 4 * x + y;
+          f[x] = 0;
+          g[x] = 0;
+      }
 
-  #pragma omp target data map(to: a[0:1024], b[0:1024], d[0:1024], e[0:1024]) map(from: c[0:1024], f[0:1024], g[0:1024]) map(tofrom: is_host)
-  {
-      #pragma omp parallel
+      #pragma omp target data map(to: a[0:1024], b[0:1024], d[0:1024], e[0:1024]) map(from: c[0:1024], f[0:1024]) map(tofrom: is_host)
       {
           #pragma omp target teams distribute nowait map(alloc: a[0:1024], b[0:1024], c[0:1024])
           for (int x = 0; x < 1024; ++x){
               c[x] = a[x] + b[x];
           }
-          #pragma omp target teams distribute nowait map(alloc: d[0:1024], e[0:1024], f[0:1024])
-          for (int x = 0; x < 1024; ++x){
-              f[x] = d[x] + e[x];
-          }
-          #pragma omp barrier
-          #pragma omp target teams distribute map(alloc: is_host, c[0:1024], f[0:1024], g[0:1024])
+          #pragma omp target teams distribute nowait map(alloc: is_host, c[0:1024], d[0:1024], e[0:1024], f[0:1024])
           for (int x = 0; x < 1024; ++x){
               is_host = omp_is_initial_device();
-              g[x] = c[x] + f[x];
+              f[x] = c[x] + d[x] + e[x];
+          }
+      }
+
+      for (int x = 0; x < 1024; ++x){
+          OMPVV_TEST_AND_SET_VERBOSE(errors, c[x] != 3 * x + 2 * y);
+          if (f[x] != 10 * x + 4 * y){
+              race_condition_found = 1
           }
       }
   }
 
-  for (int x = 0; x < 1024; ++x){
-      OMPVV_TEST_AND_SET_VERBOSE(errors, c[x] != 3 * x);
-      if (c[x] != 3 * x){
-          break;
-      }
-  }
-
-  for (int x = 0; x < 1024; ++x){
-      OMPVV_TEST_AND_SET_VERBOSE(errors, f[x] != 7 * x);
-      if (f[x] != 7 * x){
-          break;
-      }
-  }
-
-  for (int x = 0; x < 1024; ++x){
-      OMPVV_TEST_AND_SET_VERBOSE(errors, g[x] != 10 * x);
-      if (g[x] != 10 * x){
-          break;
-      }
-  }
+  OMPVV_WARNING_IF(race_conidition_found == 0, "Could not show that nowait was operating on target teams distribute construct");
 
   OMPVV_REPORT_AND_RETURN(errors);
 }
